eefef1a76d4ec0eeb8a53977d4e585b4c130d29b
Shuu Wolf <shuuwolf.home@gmail.com>
2022-05-02 19:57:59 -0300

    Editando Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import styled from 'styled-components'
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #B257e6;
-`
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello World</Title>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
