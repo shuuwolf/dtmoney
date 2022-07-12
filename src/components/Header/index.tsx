ccafa8e26822eed67a3188d0f6538a062f033412
Rafael Kauch <kauchrafael0001@gmail.com>
2022-07-12 15:16:42 -0300

    finalizando modal no react

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header(){
+interface HeaderProps{
+    onOpenNewTransationModal: () => void;
+}
+
+export function Header({onOpenNewTransationModal}: HeaderProps){
+
     return ( 
         <Container>
             <Content>
-            <img src={logoImg} alt="dt money logo" />
-            <button type="button">
-                Nova transação
-            </button>
+                <img src={logoImg} alt="dt money logo" />
+                <button 
+                type="button"
+                onClick={onOpenNewTransationModal}
+                >
+                    Nova transação
+                </button>
             </Content>  
         </Container>
     )
